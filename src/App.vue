19b1659c90377a2f91c5e1a59c59cb19d3c97c97
noelyGitHub <noely.alis209@gmail.com>
2018-10-03 12:40:39 -0500

    change img

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo1.jpg">
+    <img src="https://scontent.flim6-1.fna.fbcdn.net/v/t1.15752-9/42996884_445842525820400_3260032812211765248_n.jpg?_nc_cat=109&_nc_eui2=AeHsOrvtDwJKfFn6Tt_ZSFiruXbn8mNrvlCJFcp7XdIoSn4i-7UohWJg1UWEK0KYzdzlCf3Q4YHCkoq4oA-02WiMfH5UpnPVYGP_a5r6GRqEmQ&oh=9fe4507e50ac6e4f99936b2ad9d55581&oe=5C5F586A">
     <router-view/>
   </div>
 </template>
